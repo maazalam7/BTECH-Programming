5ff5e5da4831ffec5c06447fc38e83421a61051c
maazalam7 <maaz.alam.cs.2025@mitmeerut.ac.in>
2026-08-10 22:11:34 +0530

    Added ReverseRecursion program

---
+++ C Programs/ReverseRecursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void reverse(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    printf("%d", n % 10);
+    reverse(n / 10);
+}
+
+int main()
+{
+    int num = 12345;
+
+    printf("reverse = ");
+    reverse(num);
+
+    return 0;
+}

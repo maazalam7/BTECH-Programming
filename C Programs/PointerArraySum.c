4485c5f62a00c86201b2f91094243a6726365578
maazalam7 <maaz.alam.cs.2025@mitmeerut.ac.in>
2026-08-11 22:02:29 +0530

    Added PointerArraySum program

---
+++ C Programs/PointerArraySum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[5] = {10, 20, 30, 40, 50};
+    int *ptr = numbers;
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        sum = sum + *(ptr + i);
+    }
+
+    printf("sum = %d", sum);
+
+    return 0;
+}
